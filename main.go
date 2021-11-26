bad0e3534fdfad30683115999c9af3241d97ca0d
sj <8888@163.com>
2021-11-26 20:32:58 +0800

    init

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 	"github.com/tebeka/selenium/chrome"
 	"log"
 	"math/rand"
-	"net"
 	"net/http"
+	"runtime"
+	"time"
 )
 
-const seleniumPath = "./chromedriver.exe"
-const Core = 5
+const Core = 1
+const port = 8900
 
 var param = make([]chan string, Core)
 var result = make([]chan string, Core)
@@ -23,7 +24,11 @@ func indexHttp(w http.ResponseWriter, r *http.Request) {
 		id := rand.Intn(Core)
 		fmt.Printf("keyword=%s page=%s\n", k, p)
 		go func() {
-			param[id] <- fmt.Sprintf("https://onlinelibrary.wiley.com/action/doSearch?Ppub=&field1=Keyword&text1=%s&pageSize=20&startPage=%s", k, p)
+			if p == "1" {
+				param[id] <- fmt.Sprintf("https://onlinelibrary.wiley.com/action/doSearch?field1=Keyword&text1=%s&Ppub=", k)
+			} else {
+				param[id] <- fmt.Sprintf("https://onlinelibrary.wiley.com/action/doSearch?Ppub=&field1=Keyword&text1=%s&pageSize=20&startPage=%s", k, p)
+			}
 		}()
 		_, _ = w.Write([]byte(<-result[id]))
 	} else {
@@ -33,47 +38,48 @@ func indexHttp(w http.ResponseWriter, r *http.Request) {
 
 func startSelenium(id int) {
 
-	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:0")
-	if err != nil {
-		return
-	}
-
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		return
-	}
-	port := l.Addr().(*net.TCPAddr).Port
-	if err := l.Close(); err != nil {
-		return
-	}
-
 	selenium.SetDebug(false)
-	_, _ = selenium.NewChromeDriverService(seleniumPath, port)
+	var seleniumPath string
+	if runtime.GOOS == "windows" {
+		seleniumPath = "./chromedriver.exe"
+	} else {
+		seleniumPath = "/usr/bin/chromedriver"
+	}
+	_, _ = selenium.NewChromeDriverService(seleniumPath, port+id)
 
 	caps := selenium.Capabilities{
 		"browserName": "chrome",
 	}
-	imgCaps := map[string]interface{}{
-		"profile.managed_default_content_settings.images": 2,
-	}
+	//imgCaps := map[string]interface{}{
+	//	"profile.managed_default_content_settings.images": 2,
+	//}
 	chromeCaps := chrome.Capabilities{
-		Prefs: imgCaps,
-		Path:  "",
+		//Prefs: imgCaps,
+		Path: "",
 		Args: []string{
-			"--headless",
+			//"--headless",
 			"--no-sandbox",
+			"--disable-gpu",
+			//"blink-settings=imageEnabled=false",
 			"--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
 		},
 	}
 	caps.AddChrome(chromeCaps)
-
 	browner, e := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
-
 	if e != nil {
 		fmt.Println(e)
 	}
+	_ = browner.Get("https://onlinelibrary.wiley.com")
+	element, _ := browner.FindElement(selenium.ByTagName, "input")
+	err := element.SendKeys("hello")
+	time.Sleep(5 * time.Second)
+	err = element.SendKeys(selenium.EnterKey)
+	if err != nil {
+		return
+	}
 
 	for {
+		time.Sleep(5 * time.Second)
 		url := <-param[id]
 		_ = browner.Get(url)
 		html, _ := browner.PageSource()
@@ -88,5 +94,5 @@ func main() {
 		go startSelenium(i)
 	}
 	http.HandleFunc("/wiley", indexHttp)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
